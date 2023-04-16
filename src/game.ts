7fc153fc37509f3f4b3aced2fe7318755d67ab12
Bill Chen <billyc@gmail.com>
2023-04-16 09:27:43 -0700

    Initial typescript port. Removes template stuff and adds eslint

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,30 @@
 import * as Phaser from 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super('main')
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    preload() {
+        this.load.tilemapTiledJSON('td_tilemap', 'assets/td-map.json')
+        this.load.spritesheet(
+            'td_tiles',
+            'assets/towerDefense_tilesheet.png',
+            { frameWidth: 64, frameHeight: 64 }
+        )
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+    create() {
+        const map = this.make.tilemap({ key: 'td_tilemap' })
+        const tileset = map.addTilesetImage('td', 'td_tiles')
+        const mapLayer = map.createLayer('world', tileset, 0, 0)
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
     }
 }
 
-const config = {
+new Phaser.Game({
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
-};
-
-const game = new Phaser.Game(config);
+    width: 1024,
+    height: 768,
+    scene: Game
+});
